4ea6a43881fca4bf46b712ed3fd2cb4de3127996
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-12 03:33:45 +0600

    Lecture 3
    Completed data types in Java

---
+++ 1. Lecture/3. Data Type, Variables and Input/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Integer
+        int age = 20;
+        System.out.println(age);
+
+        // Float
+        float money = 17.50f;
+        System.out.println(money);
+        // Double
+        double myDouble = 56.09576;
+        System.out.println(myDouble);
+
+        // Character
+        char letter = 'T';
+        System.out.println(letter);
+        // String
+        String name = "Tanvir Hasan";
+        System.out.println(name + " " + age);
+
+        // Boolean
+        boolean isRight = true;
+        System.out.println(isRight);
+    }
+}
