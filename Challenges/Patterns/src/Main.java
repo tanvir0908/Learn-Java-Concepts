2ec6b95188d6d07fc39308e171196e465a9073e7
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-12 00:21:19 +0600

    Lecture 2 completed

---
+++ Challenges/Patterns/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Pattern challenges
+        System.out.println("Right half pyramid:");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* *  *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println("\nReverse right half pyramid:");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+        System.out.println("\nLeft half pyramid:");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
---
+++ Lecture/2. Java Basics/FirstProject/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Hello world
+        System.out.println("Hello world!");
+        System.out.print("I'm Tanvir Hasan Emon.\n\n");
+    }
+}
