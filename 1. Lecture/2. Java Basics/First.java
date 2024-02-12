ebe9b3219c251cd347615c02c360d8fed495be10
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-12 16:01:16 +0600

    Chapter 3 completed

---
+++ 1. Lecture/2. Java Basics/First.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class FirstProgram{
+	public static void main(String[] args){
+		System.out.print("Hello ");
+		System.out.print("World!\n");
+		System.out.print("I'm Tanvir ");
+		System.out.print("Hasan.");
+	}
+}
---
+++ 2. Challenges/3. Patterns/src/TypeConversion.java
@@ -0,0 +1,11 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        float myFloat = 5.45f;
+        int myInt = (int) myFloat;
+        System.out.println(myFloat);
+        System.out.println(myInt);
+
+        long contact = 1000;
+        System.out.println(contact);
+    }
+}
