4b0f0f0c4ebc548a4042246715cca35dc66a9ddb
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-13 22:35:36 +0600

    Check leap year code updated

--- Chapter 4/Challenge/src/18_LeapYear.java
+++ Chapter 4/Challenge/src/18_LeapYear.java
@@ -9,10 +9,12 @@ class LeapYear {
         System.out.print("Enter a year: ");
         int year = input.nextInt();
 
-        if ((year % 4 == 0 || year % 400 == 0) && year % 100 != 0) {
-            System.out.println("Result: Leap Year");
+        if (year % 400 == 0) {
+            System.out.println("This year is a leap year");
+        } else if (year % 4 != 0 || year % 100 == 0) {
+            System.out.println("This year is not a leap year.");
         } else {
-            System.out.println("Result: Not Leap Year");
+            System.out.println("This year is a leap year");
         }
     }
 }
