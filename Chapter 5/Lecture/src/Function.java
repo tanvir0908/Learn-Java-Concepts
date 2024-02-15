a9a533f936002e0eb6dca8aa67f3268063d2d448
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-15 20:05:52 +0600

    Print patterns using loops completed

--- Chapter 5/Lecture/src/Function.java
+++ Chapter 5/Lecture/src/Function.java
@@ -2,9 +2,37 @@ public class Function {
     public static void main(String[] args) {
         System.out.println("First pattern: ");
         firstPattern();
+
+//        System.out.println("Second pattern: ");
+//        secondPattern();
+//
+//        System.out.println("Third pattern: ");
+//        thirdPattern();
     }
 
     public static void firstPattern() {
+//      print pattern using loops
+        int rows = 0;
+        while (rows <= 20) {
+            int starCount = 0;
+            while (starCount <= rows) {
+                System.out.print("* ");
+                starCount++;
+            }
+            System.out.println();
+            rows++;
+        }
+    }
+
+    public static void secondPattern() {
+        System.out.println("* * * * *");
+        System.out.println("* * * * ");
+        System.out.println("* * *   ");
+        System.out.println("* *     ");
+        System.out.println("*       \n");
+    }
+
+    public static void thirdPattern() {
         System.out.println("        *");
         System.out.println("      * *");
         System.out.println("    * * *");
