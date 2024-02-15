b493688c455cfa707c5e0f960de52512a5970cc0
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-15 21:26:50 +0600

    Update 28_MultiplicationTable.java

--- Chapter 5/Challange/src/28_MultiplicationTable.java
+++ Chapter 5/Challange/src/28_MultiplicationTable.java
@@ -2,27 +2,21 @@ import java.util.Scanner;
 
 class MultiplicationTable {
     public static void main(String[] args) {
-        greetUser();
-        int number = userInput();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Multiplication Table");
+
+        System.out.print("Please enter a number: ");
+        int number = input.nextInt();
+
         multiplicationTable(number);
     }
 
-    public static void greetUser() {
-        System.out.println("Welcome to Multiplication Table");
-    }
-
-    public static int userInput() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter a number: ");
-        int number = input.nextInt();
-        return number;
-    }
-
     public static void multiplicationTable(int number) {
-        int range = 0;
+        int range = 1;
         System.out.println("\nMultiplication Table of: " + number);
         while (range <= 10) {
-            System.out.println(number + " * " + range + " = " + (range * number));
+            System.out.println(number + " x " + range + " = " + (range * number));
             range++;
         }
     }
