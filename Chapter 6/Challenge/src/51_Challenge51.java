463efc1ac93e4bec4c2a7d379fc0ab7496d5af2b
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-21 01:02:52 +0600

    Challenge-51 updated

--- Chapter 6/Challenge/src/51_Challenge51.java
+++ Chapter 6/Challenge/src/51_Challenge51.java
@@ -1,11 +1,14 @@
 class Challenge51 {
     public static void main(String[] args) {
-        Book myBook = new Book();
-        // instance methods
-        myBook.borrowBook();
-        myBook.returnBook();
+        Book designOfThings = new Book("Design", "Tanvir", "1");
+        Book myBook = new Book("1");
 
-        // static methods
-        Book.getTotalBooks();
+        System.out.println("Total number of books: " + Book.getTotalBooks());
+
+        designOfThings.borrowBook();
+        myBook.borrowBook();
+        designOfThings.borrowBook();
+        designOfThings.returnBook();
+        designOfThings.returnBook();
     }
 }
--- Chapter 6/Challenge/src/Book.java
+++ Chapter 6/Challenge/src/Book.java
@@ -1,23 +1,57 @@
-public class Book {
-    // instance variables
-    String title;
-    String author;
-    boolean isbn;
-
+class Book {
     // static variables
     static int totalBooks;
 
-    // instance methods
-    public void borrowBook() {
-        System.out.println("This is inside borrow book method");
+    // instance variables
+    String title;
+    String author;
+    String isbn;
+    boolean isBorrowed;
+
+    // static block
+    static {
+        totalBooks = 0;
     }
 
-    public void returnBook() {
-        System.out.println("This is return book method");
+    // object init
+    {
+        totalBooks++;
+    }
+
+    // constructor
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    Book(String isbn) {
+        this("Unknown", "Unknown", isbn);
     }
 
     // static method
-    public static void getTotalBooks() {
-        System.out.println("This is inside get total books method");
+    static int getTotalBooks() {
+        return totalBooks;
+    }
+
+    // instance methods
+    public void borrowBook() {
+        if (isBorrowed) {
+            System.out.println("Book is already borrowed");
+        } else {
+            this.isBorrowed = true;
+            System.out.println("Enjoy " + this.title);
+        }
+
+    }
+
+    public void returnBook() {
+        if (isBorrowed) {
+            this.isBorrowed = false;
+            System.out.println("Hope ypu enjoyed!");
+        } else {
+            System.out.println("This book is already in stock");
+        }
+
     }
 }
