71ca5d1c1036fc6ba127f570c6f516f7ff263674
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-21 22:59:18 +0600

    Challenge- 55 absolute number completed

--- Chapter 7/Challenge/src/Challenge_55.java
+++ Chapter 7/Challenge/src/Challenge_55.java
@@ -1,1 +1,19 @@
-class FindAbsolute
+import java.util.Scanner;
+
+class FindAbsolute {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to absolute number finder");
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        int absoluteNumber = findAbsoluteNumber(num);
+        System.out.println("Absolute number is: " + absoluteNumber);
+    }
+
+    public static int findAbsoluteNumber(int num) {
+        int absoluteNumber = num < 0 ? num * -1 : num;
+        return absoluteNumber;
+    }
+}
