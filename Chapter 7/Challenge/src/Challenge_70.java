0f1fa74bff570df465bca54fa217c1c182b32f3f
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-23 22:24:22 +0600

    Update Challenge_70.java

--- Chapter 7/Challenge/src/Challenge_70.java
+++ Chapter 7/Challenge/src/Challenge_70.java
@@ -1,18 +1,21 @@
 class Student {
     String name;
     int age;
+    String rollNumber;
 
-    Student(String name, int age) {
+    Student(String name, int age, String rollNumber) {
         this.name = name;
         this.age = age;
+        this.rollNumber = rollNumber;
     }
 
+    @Override
     public String toString() {
-        return "Student name: " + this.name + "\nStudent age: " + age;
+        return "Student name: " + this.name + "\nStudent age: " + age + "\nStudent Id: " + rollNumber;
     }
 
     public static void main(String[] args) {
-        Student student1 = new Student("Tanvir", 24);
+        Student student1 = new Student("Tanvir", 24, "011181077");
         System.out.println(student1);
     }
 }
