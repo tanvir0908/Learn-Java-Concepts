b1fb59b13e88d1f464bfeba262a23f3371b68980
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-24 21:22:06 +0600

    Challenge-76 Final operator completed

---
+++ Chapter 7/Challenge/src/Challenge_76.java
@@ -0,0 +1,11 @@
+class FinalField {
+    final String name;
+    final int noOfWheels;
+    final String enginePower;
+
+    FinalField(String name, int noOfWheels, String enginePower) {
+        this.name = name;
+        this.noOfWheels = noOfWheels;
+        this.enginePower = enginePower;
+    }
+}
