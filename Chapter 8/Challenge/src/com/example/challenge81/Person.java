ad5ed8002091d837a9f434174ab6b1694ab3a975
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-27 13:32:58 +0600

    Challenge-81 completed

---
+++ Chapter 8/Challenge/src/com/example/challenge81/Person.java
@@ -0,0 +1,21 @@
+package com.example.challenge81;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
