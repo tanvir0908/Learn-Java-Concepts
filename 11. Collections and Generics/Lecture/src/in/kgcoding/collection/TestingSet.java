2fbff9c6626d637c32a63eb769b9e2722430adbd
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-03-01 21:43:31 +0600

    Completed Set Collection

--- 11. Collections and Generics/Lecture/src/in/kgcoding/collection/TestingSet.java
+++ 11. Collections and Generics/Lecture/src/in/kgcoding/collection/TestingSet.java
@@ -5,12 +5,24 @@ import java.util.Set;
 
 public class TestingSet {
     public static void main(String[] args) {
+        // create an object of a set class
         Set<String> names = new HashSet<>();
 
-        System.out.println(names.add("Tanvir"));
-        System.out.println(names.add("Hasan"));
-        System.out.println(names.add("Emon"));
-        
+        // insert element
+        names.add("Tanvir"); // returns true
+        names.add("Tanvir"); // returns false
+        names.add("Hasan"); // returns true
+        names.add("Emon"); // returns true
+
+
+        Utility.displayCollection(names);
+
+        // check item exist or not
+        System.out.println(names.contains("Tanvir"));
+
+        // remove item
+        names.remove("Hasan");
+
         Utility.displayCollection(names);
     }
 }
