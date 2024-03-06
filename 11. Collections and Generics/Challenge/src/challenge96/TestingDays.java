2aa5434ec6fe55cf6d54c5b1ac860ad1ddac0d33
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-03-06 17:13:52 +0600

    Challenge-96 completed

---
+++ 11. Collections and Generics/Challenge/src/challenge96/TestingDays.java
@@ -0,0 +1,9 @@
+package challenge96;
+
+public class TestingDays {
+    public static void main(String[] args) {
+        for (WeekDays days : WeekDays.values()) {
+            System.out.println(days);
+        }
+    }
+}
---
+++ 11. Collections and Generics/Challenge/src/challenge96/WeekDays.java
@@ -0,0 +1,5 @@
+package challenge96;
+
+public enum WeekDays {
+    Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday;
+}
