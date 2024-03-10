98f3b595c61cf02e3e26522002dcd6e668b3cdf6
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-03-10 19:45:25 +0600

    Challenge-105 completed

---
+++ 12. Multi Threading and Executor Service/Challenge/src/challenge104/Executor.java
@@ -0,0 +1,32 @@
+package challenge104;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Executor {
+    public static void main(String[] args) {
+        try (ExecutorService service = Executors.newFixedThreadPool(3)) {
+            Factorial n1 = new Factorial(1);
+            Factorial n2 = new Factorial(2);
+            Factorial n3 = new Factorial(3);
+            Factorial n4 = new Factorial(4);
+            Factorial n5 = new Factorial(5);
+
+            Future<Integer> f1 = service.submit(n1);
+            Future<Integer> f2 = service.submit(n2);
+            Future<Integer> f3 = service.submit(n3);
+            Future<Integer> f4 = service.submit(n4);
+            Future<Integer> f5 = service.submit(n5);
+
+            System.out.println(f1.get());
+            System.out.println(f2.get());
+            System.out.println(f3.get());
+            System.out.println(f4.get());
+            System.out.println(f5.get());
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ 12. Multi Threading and Executor Service/Challenge/src/challenge104/Factorial.java
@@ -0,0 +1,25 @@
+package challenge104;
+
+import java.util.concurrent.Callable;
+
+public class Factorial implements Callable<Integer> {
+
+    private final int number;
+
+    Factorial(int number) {
+        this.number = number;
+    }
+
+    private int calculateFactorial() {
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return calculateFactorial();
+    }
+}
